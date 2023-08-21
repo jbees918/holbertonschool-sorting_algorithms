d997d2383bcbbb4555edd281af3b593f0a8786a7
jbees918 <5590@holbertonstudents.com>
2023-08-21 18:34:39 -0500

    selection

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,31 +1,49 @@
 #include "sort.h"
-
 /**
- * selection_sort - sorts an array of integers in
- * ascending order using the selection sort algorithm
- * @array: array
- * @size: size of the array
+ * swap - swaps two variables
+ * @i: first one to swap
+ * @j: second one to swap
  */
 
+void swap(int *i, int *j)
+{
+	int tmp = *i;
+
+	*i = *j;
+	*j = tmp;
+}
+
+
+
+/**
+ * selection_sort - sort array of int ascending order using selection algorithm
+ * @array: array to sort
+ * @size: size of the array
+ */
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j, min;
-	int temp;
+	unsigned int i, j, idx;
+	int min, flag = 0;
 
+	if (array == NULL)
+		return;
 	for (i = 0; i < size - 1; i++)
 	{
-		min = i;
-		for (j = i + 1; j < size; j++)
+		min = array[i];
+		for (j = i; j < size; j++)
 		{
-			if (array[j] < array[min])
-				min = j;
+			if (array[j] < min)
+			{
+				min = array[j];
+				idx = j;
+				flag = 1;
+			}
+			if ((flag == 1) && (j == size - 1))
+			{
+				swap(&array[idx], &array[i]);
+				print_array(array, size);
+			}
 		}
-		if (min != i)
-		{
-			temp = array[i];
-			array[i] = array[min];
-			array[min] = temp;
-			print_array(array, size);
-		}
+		flag = 0;
 	}
 }
